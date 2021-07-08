4e21b64badd6183381625a0ec3b155fd669790a3
matt <egginton@gmail.com>
2021-07-08 13:39:54 -0230

    Delete __test__ directory

--- __test__/formHandler.test.js
+++
@@ -1,9 +0,0 @@
-// Import the js file to test
-import { handleSubmit } from '../src/client/js/formHandler';
-// The describe() function
-describe('Testing the submit', () => {
-  // The test() function
-  test('Testing handleSubmit() ', () => {
-    expect(handleSubmit).toBeDefined();
-  });
-});
